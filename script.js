00ffe7070bf395b83c0e13d8ff49f637ba38a065
Your Name <you@example.com>
2022-10-21 23:45:54 +0600

    first

---
+++ script.js
@@ -0,0 +1,98 @@
+
+const box = document.querySelector(".box");
+const b1 = document.querySelector(".one");
+b1.addEventListener("click", ()=>{
+    box.style.top = '90%'
+    box.innerText='first floor'
+   
+})
+const b2 = document.querySelector(".two");
+b2.addEventListener("click", ()=>{
+    box.style.top = '85%'
+    box.innerText='second floor'
+   
+})
+const b3 = document.querySelector(".three");
+b3.addEventListener("click", ()=>{
+    box.style.top = '80%'
+    box.innerText='third floor'
+   
+})
+const b4 = document.querySelector(".four");
+b4.addEventListener("click", ()=>{
+    box.style.top = '75%'
+    box.innerText='fourth floor'
+   
+})
+const b5 = document.querySelector(".five");
+b5.addEventListener("click", ()=>{
+    box.style.top = '70%'
+    box.innerText='fifth floor'
+   
+})
+const b6 = document.querySelector(".six");
+b6.addEventListener("click", ()=>{
+    box.style.top = '65%'
+    box.innerText='sixth floor'
+   
+})
+const b7 = document.querySelector(".seven");
+b7.addEventListener("click", ()=>{
+    box.style.top = '60%'
+    box.innerText='seventh floor'
+   
+})
+const b8 = document.querySelector(".eight");
+b8.addEventListener("click", ()=>{
+    box.style.top = '55%'
+    box.innerText='eightth floor'
+   
+})
+const b9 = document.querySelector(".nine");
+b9.addEventListener("click", ()=>{
+    box.style.top = '50%'
+    box.innerText='nineth floor'
+   
+})
+const b10 = document.querySelector(".ten");
+b10.addEventListener("click", ()=>{
+    box.style.top = '45%'
+    box.innerText='tenth floor'
+   
+})
+const b11 = document.querySelector(".eleven");
+b11.addEventListener("click", ()=>{
+    box.style.top = '40%'
+    box.innerText='eleventh floor'
+   
+})
+const b12 = document.querySelector(".twelve");
+b12.addEventListener("click", ()=>{
+    box.style.top = '35%'
+    box.innerText='twelveth floor'
+   
+})
+const b13 = document.querySelector(".thirt");
+b13.addEventListener("click", ()=>{
+    box.style.top = '30%'
+    box.innerText='thirteenth floor'
+   
+})
+const b14 = document.querySelector(".fourt");
+b14.addEventListener("click", ()=>{
+    box.style.top = '25%'
+    box.innerText='fourteenth floor'
+   
+})
+const b15 = document.querySelector(".fift");
+b15.addEventListener("click", ()=>{
+    box.style.top = '20%'
+    box.innerText='fifteenth floor'
+   
+})
+const b16 = document.querySelector(".sixt");
+b16.addEventListener("click", ()=>{
+    box.style.top = '15%'
+    box.innerText='sixteenth floor'
+   
+})
